2c292cc22cf6e9ea2ab586dc3cfa814adc63b810
Logan <coates.logan1125@gmail.com>
2022-01-17 13:25:57 -0700

    sidebar

--- fun/src/components/Navbar/Sidebar/index.js
+++ fun/src/components/Navbar/Sidebar/index.js
@@ -1,14 +1,35 @@
-import React from 'react'
-import { SidebarContainer, Icon, CloseIcon } from './SidebarElements'
+import React from "react";
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink,
+  SideBtnWrap,
+  SidebarRoute,
+} from "./SidebarElements";
 
 const Sidebar = () => {
-    return (
-        <SidebarContainer>
-           <Icon>
-               <CloseIcon />
-            </Icon> 
-        </SidebarContainer>
-    )
-}
+  return (
+    <SidebarContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="about">About</SidebarLink>
+          <SidebarLink to="discover">Discover</SidebarLink>
+          <SidebarLink to="services">Services</SidebarLink>
+          <SidebarLink to="signup">Sign Up</SidebarLink>
+          <SidebarLink to="about">About</SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to="/signin">Sign In</SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
